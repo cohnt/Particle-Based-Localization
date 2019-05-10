6a5bcf954f3356f18a69dab6a18fed99d1be265c
Thomas Cohn <cohnt@umich.edu>
2019-05-10 16:54:03 -0400

    TF: Move checking if a transformation is available to its own function.

--- transformer.py
+++ transformer.py
@@ -20,6 +20,14 @@ class Transformer:
 
 		self.tfListener = tf.TransformListener()
 
+	def canTransform(self, frame1, frame2, staticFrame, stamp, timeout=0.1):
+		try:
+			self.tfListener.waitForTransformFull(frame1, stamp, frame2, stamp, staticFrame, rospy.Duration(timeout))
+			return True
+		except tf.Exception as e:
+			print "TF Exception: %s" % str(e)
+			return False
+
 	def transform(self, points, stamp):
 		horizFOV = self.FOV[0]
 		vertFOV = self.FOV[1]
@@ -32,42 +40,36 @@ class Transformer:
 
 		newPoints = []
 
-		waiting = False
-		while True:
-			try:
-				self.tfListener.waitForTransformFull("/odom", stamp, "/head_camera_rgb_optical_frame", stamp, "/odom", rospy.Duration(0.5))
-				pos, quat = self.tfListener.lookupTransformFull("/odom", stamp, "/head_camera_rgb_optical_frame", stamp, "/odom")
-				# stamp = self.tfListener.getLatestCommonTime("/head_camera_rgb_optical_frame", "/odom")
-				matTransform = self.tfListener.fromTranslationRotation(pos, quat)
+		if self.canTransform("/odom", "/head_camera_rgb_optical_frame", "/odom", stamp):
+			pos, quat = self.tfListener.lookupTransformFull("/odom", stamp, "/head_camera_rgb_optical_frame", stamp, "/odom")
+			matTransform = self.tfListener.fromTranslationRotation(pos, quat)
 
-				print "Got transform!"
+			print "Got transform!"
 
-				startPoint = np.array([0, 0, 0, 1])
-				startPoint = np.matmul(matTransform, startPoint)
+			startPoint = np.array([0, 0, 0, 1])
+			startPoint = np.matmul(matTransform, startPoint)
 
-				for point in points:
-					endPixel = np.array(point)
+			for point in points:
+				endPixel = np.array(point)
 
-					centeredPixel = endPixel - (self.imgShape / 2)
+				centeredPixel = endPixel - (self.imgShape / 2)
 
-					endAngs = angles * centeredPixel
+				endAngs = angles * centeredPixel
 
-					# print "Angs: %f \t\t\t %f" % (endAngs[0], endAngs[1])
+				# print "Angs: %f \t\t\t %f" % (endAngs[0], endAngs[1])
 
-					endRay = [math.sin(endAngs[0]), -43]
+				endRay = [math.sin(endAngs[0]), -43]
 
-					h1 = math.sqrt(1 + endRay[0]**2)
+				h1 = math.sqrt(1 + endRay[0]**2)
 
-					endRay[1] = h1 * math.sin(endAngs[1])
+				endRay[1] = h1 * math.sin(endAngs[1])
 
-					endPoint = np.array([endRay[0], endRay[1], 1, 1])
-					endPoint = np.matmul(matTransform, endPoint)
+				endPoint = np.array([endRay[0], endRay[1], 1, 1])
+				endPoint = np.matmul(matTransform, endPoint)
 
-					newPoints.append(endPoint.copy())
-				break
-			except:
-				sys.stdout.write("Skipping this image.")
-				sys.stdout.flush()
-				return (None, None, False)
+				newPoints.append(endPoint.copy())
 
-		return (startPoint, newPoints, True)
+			return (startPoint, newPoints, True)
+		else:
+			print "Skipping this image -- TF not ready."
+			return (None, None, False)
